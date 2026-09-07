cea864772061b62453ec5b98b4eff156f58fa3c5
Arihant Jha <arihantjha20@gmail.com>
2026-09-07 23:42:07 +0530

    Delete Assignment 1 directory

--- Assignment 1/crypto/hashing.py
+++
@@ -1,7 +0,0 @@
-import hashlib
-import json
-
-# Create a SHA-256 hash of the transaction data
-def create_hash(transaction_data):
-    transaction_string = json.dumps(transaction_data, sort_keys=True).encode()
-    return hashlib.sha256(transaction_string).hexdigest()
